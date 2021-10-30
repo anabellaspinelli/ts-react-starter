c2aa7fd3113da980c8cba580d6b12f5d13d17dcb
Anabella <anabellaspinelli@gmail.com>
2021-10-30 12:30:01 +0200

    Initial commit with linter config

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    node: true,
+    commonjs: true,
+    browser: true,
+    es6: true,
+  },
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'cypress',
+    'eslint-plugin-chai-friendly',
+    'jsx-a11y',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:chai-friendly/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier',
+  ],
+  settings: { react: { version: 'detect' } },
+  rules: {
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'react/prop-types': 'off',
+    'react-hooks/exhaustive-deps': 'error',
+    'react/self-closing-comp': 'warn',
+  },
+};
--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(): JSX.Element | null {
   return (
-    <div className="App">
+    <div className="app">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
